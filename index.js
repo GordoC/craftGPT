12adb18633e4f608e94f86587b2a60bd541fce09
GordoC <gordonch99@gmail.com>
2023-04-04 23:45:51 -0700

    bot detecting chat

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const mineflayer = require('mineflayer')
 
 const bot = mineflayer.createBot({
     host: 'localhost',
-    port: 0,
+    port: 62239,
     username: 'CraftGPT'
 })
+
+// Log errors and kick reasons:
+bot.on('kicked', console.log)
+bot.on('error', console.log)
+
+// User chatting with bot, activated by starting the message with '?'
+bot.on('chat', (username, message) => {
+    if (username === bot.username) return
+    if (message.length < 1) return
+    if (message.substring(0,1) != '?') return
+    bot.chat(message)
+})
+  
