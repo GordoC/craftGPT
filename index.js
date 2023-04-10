9d03af6da7ca5189aedec10d2752bea36f4099d1
GordoC <gordonch99@gmail.com>
2023-04-09 22:40:14 -0700

    go to spawn and find harvestable crop

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ bot.loadPlugin(pathfinder);
 
 bot.on('spawn', () => {
     bot.chat("hello!")
+
+    // Loads pathfinder into bot
+    // Done once per program unless different Movements configurations
+    const mcData = require('minecraft-data')(bot.version)
+    const movements = new Movements(bot, mcData)
+    movements.scafoldingBlocks = [] // Removes bot from scafolding to player
+
+    bot.pathfinder.setMovements(movements)
 })
 
 // User chatting with bot, activated by starting the message with '?'
@@ -28,9 +36,13 @@ bot.on('chat', (username, message) => {
     if (message.substring(0,1) != '?') return
     // bot.chat(message)
     // followPlayer(username)
-    findFarmlands()
+    if (message == "?hi") findFarmland()
+    if (message == "?bye") findHarvestableFarmland('wheat')
+    if (message == "?reset") goToSpawn()
 })
 
+
+// Follow player based on username
 function followPlayer(username) {
     const player = bot.players[username]
 
@@ -39,28 +51,25 @@ function followPlayer(username) {
         return
     }
 
-    // Loads pathfinder into bot
-    // Done once per program unless different Movements configurations
-    const mcData = require('minecraft-data')(bot.version)
-    const movements = new Movements(bot, mcData)
-    movements.scafoldingBlocks = [] // Removes bot from scafolding to player
-
-    bot.pathfinder.setMovements(movements)
-
     const goal = new GoalFollow(player.entity, 1) // 1 = # of blocks away
     bot.pathfinder.setGoal(goal, true) // true if entity is dynamic (moving), constantly checks new position
 }
 
-function findFarmlands() {
-    // Loads pathfinder into bot
-    const mcData = require('minecraft-data')(bot.version)
-    const movements = new Movements(bot, mcData)
-    movements.scafoldingBlocks = [] 
-    bot.pathfinder.setMovements(movements)
+// Go to spawn: 0, -60, 0
+function goToSpawn() {
+    const x = 0
+    const y = -60
+    const z = 0
+    const goal = new GoalBlock(x, y, z)
+    bot.pathfinder.setGoal(goal)
+}
 
-    // Finds closest farmland
+// Finds closest farmland
+function findFarmland() {
     const farmland = bot.findBlock({
-        matching: mcData.blocksByName.farmland.id,
+        matching: (block)=>{
+			return block.name === 'farmland'
+		},
         maxDistance: 32
     })
 
@@ -73,6 +82,28 @@ function findFarmlands() {
     const x = farmland.position.x
     const y = farmland.position.y + 1
     const z = farmland.position.z
+    const goal = new GoalBlock(x, y, z)
+    bot.pathfinder.setGoal(goal)
+}
+
+// Finds closest harvestable farmland based on crop name
+function findHarvestableFarmland(cropName) {
+    const harvestableFarmLand = bot.findBlock({
+        matching: (block)=>{
+			return (block.name == cropName && block.metadata == 7)
+		},
+        maxDistance: 32
+    })
+
+    if (!harvestableFarmLand) {
+        bot.chat("No harvestable " + cropName)
+        return
+    }
+
+    // Set goal to 1 block above harvestable farmland in bot
+    const x = harvestableFarmLand.position.x
+    const y = harvestableFarmLand.position.y + 1
+    const z = harvestableFarmLand.position.z
     const goal = new GoalBlock(x, y, z)
     bot.pathfinder.setGoal(goal)
 }
