9e967c100036132ace680710bb83a7b3dc84ad24
GordoC <gordonch99@gmail.com>
2023-04-09 22:03:08 -0700

    Find farmland block

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const mineflayer = require('mineflayer')
 const { pathfinder, Movements, goals } = require("mineflayer-pathfinder");
 const GoalFollow = goals.GoalFollow
+const GoalBlock = goals.GoalBlock
+
 
 const bot = mineflayer.createBot({
     host: 'localhost',
@@ -16,7 +18,7 @@ bot.on('error', console.log)
 bot.loadPlugin(pathfinder);
 
 bot.on('spawn', () => {
-    bot.chat("Hello!")
+    bot.chat("hello!")
 })
 
 // User chatting with bot, activated by starting the message with '?'
@@ -25,7 +27,8 @@ bot.on('chat', (username, message) => {
     if (message.length < 1) return
     if (message.substring(0,1) != '?') return
     // bot.chat(message)
-    followPlayer(username)
+    // followPlayer(username)
+    findFarmlands()
 })
 
 function followPlayer(username) {
@@ -43,10 +46,34 @@ function followPlayer(username) {
     movements.scafoldingBlocks = [] // Removes bot from scafolding to player
 
     bot.pathfinder.setMovements(movements)
-    
 
     const goal = new GoalFollow(player.entity, 1) // 1 = # of blocks away
     bot.pathfinder.setGoal(goal, true) // true if entity is dynamic (moving), constantly checks new position
 }
 
+function findFarmlands() {
+    // Loads pathfinder into bot
+    const mcData = require('minecraft-data')(bot.version)
+    const movements = new Movements(bot, mcData)
+    movements.scafoldingBlocks = [] 
+    bot.pathfinder.setMovements(movements)
+
+    // Finds closest farmland
+    const farmland = bot.findBlock({
+        matching: mcData.blocksByName.farmland.id,
+        maxDistance: 32
+    })
+
+    if (!farmland) {
+        bot.chat("No farmlands")
+        return
+    }
+
+    // Set goal to 1 block above farmland in bot
+    const x = farmland.position.x
+    const y = farmland.position.y + 1
+    const z = farmland.position.z
+    const goal = new GoalBlock(x, y, z)
+    bot.pathfinder.setGoal(goal)
+}
 
